d2fb08acddc93dda54410247246c80ee7096d877
Frostzie <136027000+Frostzie@users.noreply.github.com>
2025-03-21 22:40:40 +0200

    Compleatly reworked the DungeonCommands

--- src/main/kotlin/io/github/frostzie/skyfall/commands/CommandManager.kt
+++ src/main/kotlin/io/github/frostzie/skyfall/commands/CommandManager.kt
@@ -2,11 +2,13 @@ package io.github.frostzie.skyfall.commands
 
 object CommandManager {
     fun loadCommands() {
+        // Always On Commands
         AlwaysOnCommands.openConfig()
         AlwaysOnCommands.sfColor()
         AlwaysOnCommands.sfHelp()
 
+        // Dungeon Commands
         DungeonCommands.requeue()
-        DungeonCommands.shortening()
+        DungeonCommands.floorCommands()
     }
 }

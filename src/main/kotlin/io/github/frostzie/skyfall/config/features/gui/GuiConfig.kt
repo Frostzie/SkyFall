6f06430bb52ed9f50bd196bd4445e5d2bb8acc72
Frostzie <gamerboy1000live@gmail.com>
2025-05-26 22:02:13 +0300

    Added Hud editor and finished updating build.gradle.kts to fully use libs now

--- src/main/kotlin/io/github/frostzie/skyfall/config/features/gui/GuiConfig.kt
+++ src/main/kotlin/io/github/frostzie/skyfall/config/features/gui/GuiConfig.kt
@@ -1,14 +1,25 @@
 package io.github.frostzie.skyfall.config.features.gui
 
 import com.google.gson.annotations.Expose
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorKeybind
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 import org.lwjgl.glfw.GLFW
 
 class GuiConfig {
-
+//TODO: add text box for info how to use the resizing feature and put it as a tiny icon symbol in the top right corner of the screen
     @Expose
     @ConfigOption(name = "Open Config Key", desc = "Keybind to open the config")
     @ConfigEditorKeybind(defaultKey = GLFW.GLFW_KEY_UNKNOWN)
     var openConfigKey: Int = GLFW.GLFW_KEY_UNKNOWN
+
+    @Expose
+    @ConfigOption(name = "Open Hud Editor", desc = "Keybind to open the Hud Editor")
+    @ConfigEditorKeybind(defaultKey = GLFW.GLFW_KEY_UNKNOWN)
+    var openHudEditorKey: Int = GLFW.GLFW_KEY_UNKNOWN
+
+    @Expose
+    @ConfigOption(name = "More Hud Editor", desc = "Adds more Hud Editor customization options.")
+    @ConfigEditorBoolean
+    var fullHudEditor = false
 }

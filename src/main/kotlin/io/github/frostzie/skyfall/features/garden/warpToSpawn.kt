7d9e5f4e4182cbd06dc969502a694c186978c032
Frostzie <gamerboy1000live@gmail.com>
2025-07-09 01:21:14 +0300

    Warp to Garden Spawn keybind.

---
+++ src/main/kotlin/io/github/frostzie/skyfall/features/garden/warpToSpawn.kt
@@ -0,0 +1,45 @@
+package io.github.frostzie.skyfall.features.garden
+
+import io.github.frostzie.skyfall.SkyFall
+import io.github.frostzie.skyfall.features.Feature
+import io.github.frostzie.skyfall.features.IFeature
+import io.github.frostzie.skyfall.utils.KeyboardManager.isKeyClicked
+import io.github.frostzie.skyfall.utils.SimpleTimeMark
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import org.lwjgl.glfw.GLFW
+import kotlin.time.Duration.Companion.milliseconds
+
+@Feature(name = "Warp to Garden Spawn")
+object warpToSpawn : IFeature {
+    override var isRunning = false
+    private var lastCommandTime = SimpleTimeMark.farPast()
+    private val requeueKey get() = SkyFall.feature.garden.warpToSpawn
+
+    override fun shouldLoad(): Boolean {
+        return requeueKey != GLFW.GLFW_KEY_UNKNOWN
+    }
+
+    init {
+        ClientTickEvents.END_CLIENT_TICK.register { client ->
+            if (client.currentScreen == null) {
+                if (requeueKey != GLFW.GLFW_KEY_UNKNOWN) {
+                    if (requeueKey.isKeyClicked() && lastCommandTime.passedSince() >= 350.milliseconds) {
+                        val player = client.player
+                        if (player != null) {
+                            player.networkHandler.sendChatCommand("warp garden")
+                            lastCommandTime = SimpleTimeMark.now()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun init() {
+        isRunning = true
+    }
+
+    override fun terminate() {
+        isRunning = false
+    }
+}

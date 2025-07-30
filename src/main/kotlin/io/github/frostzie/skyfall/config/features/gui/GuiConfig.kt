aee83abe879f6f753b51bb2ff098d306569b8b4e
Frostzie <136027000+Frostzie@users.noreply.github.com>
2025-07-30 04:53:24 +0300

    Update to Beta 0.2.3. Changed text in config to be readable and whispers to powder when needed.

--- src/main/kotlin/io/github/frostzie/skyfall/config/features/gui/GuiConfig.kt
+++ src/main/kotlin/io/github/frostzie/skyfall/config/features/gui/GuiConfig.kt
@@ -1,7 +1,9 @@
 package io.github.frostzie.skyfall.config.features.gui
 
 import com.google.gson.annotations.Expose
+import io.github.frostzie.skyfall.config.ConfigGuiManager
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorButton
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorKeybind
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 import org.lwjgl.glfw.GLFW
@@ -12,6 +14,10 @@ class GuiConfig {
     @ConfigOption(name = "Open Config Key", desc = "Keybind to open the config")
     @ConfigEditorKeybind(defaultKey = GLFW.GLFW_KEY_UNKNOWN)
     var openConfigKey: Int = GLFW.GLFW_KEY_UNKNOWN
+
+    @ConfigOption(name = "Open Hud Editor", desc = "Press to Open Hud Editor")
+    @ConfigEditorButton(buttonText = "Click!")
+    var openHudEditorButton: Runnable = Runnable(ConfigGuiManager::openHudEditor)
 
     @Expose
     @ConfigOption(name = "Open Hud Editor", desc = "Keybind to open the Hud Editor")

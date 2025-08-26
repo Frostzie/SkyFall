6794120add958762232741d25f93b62bc7b27738
Frostzie <136027000+Frostzie@users.noreply.github.com>
2025-08-26 14:17:00 +0300

    logger message showing os

--- src/main/kotlin/io/github/frostzie/datapackide/utils/JavaFXInitializer.kt
+++ src/main/kotlin/io/github/frostzie/datapackide/utils/JavaFXInitializer.kt
@@ -11,6 +11,7 @@ object JavaFXInitializer {
     private var setImplicitExitMethod: Method? = null
 
     init {
+        logger.info("os.name=${System.getProperty("os.name")}, os.arch=${System.getProperty("os.arch")}")
         // Set fallback and verbose debugging BEFORE JavaFX starts
         System.setProperty("prism.order", "es2,sw")   // try hardware ES2, then software
         System.setProperty("prism.verbose", "true")  // prints which pipeline is picked/tried

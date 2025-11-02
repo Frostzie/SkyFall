5bdf17dc88b1feef856a471c9ae5c804ca684f8e
Frostzie <136027000+Frostzie@users.noreply.github.com>
2025-11-02 19:28:23 +0200

    Moved JS Console logger from EditorBridge to LoggerProvider. Removed old handler and Module for TextEditor.

--- src/main/kotlin/io/github/frostzie/datapackide/modules/main/EditorBridge.kt
+++ src/main/kotlin/io/github/frostzie/datapackide/modules/main/EditorBridge.kt
@@ -33,7 +33,7 @@ class EditorBridge(
                     window.setMember("kotlinBridge", KotlinHandler())
 
                     // Setup console logging interception
-                    setupConsoleLogging()
+                    LoggerProvider.setupConsoleLogging(webView)
                     // Disable context menu
                     webView.isContextMenuEnabled = false
 
@@ -56,53 +56,6 @@ class EditorBridge(
         } catch (e: Exception) {
             logger.error("Error loading content after editor ready", e)
         }
-    }
-
-    //TODO: Move this away from here
-    /**
-     * Sets up JavaScript console logging to redirect to logger
-     */
-    private fun setupConsoleLogging() {
-        webView.engine.executeScript(
-            """
-            (function() {
-                const originalLog = console.log;
-                const originalError = console.error;
-                const originalWarn = console.warn;
-                const originalInfo = console.info;
-
-                function formatArgs(args) {
-                    return args.map(arg => {
-                        if (arg instanceof Error) {
-                            return arg.stack || arg.message;
-                        }
-                        if (typeof arg === 'object' && arg !== null) {
-                            try { return JSON.stringify(arg, null, 2); } catch (e) { return '[Unserializable Object]'; }
-                        }
-                        return String(arg);
-                    }).join(' ');
-                }
-
-                console.log = function(...args) { 
-                    if (window.kotlinBridge) kotlinBridge.consoleLog(formatArgs(args)); 
-                    originalLog.apply(console, args); 
-                };
-                console.error = function(...args) { 
-                    if (window.kotlinBridge) kotlinBridge.consoleError(formatArgs(args)); 
-                    originalError.apply(console, args); 
-                };
-                console.warn = function(...args) { 
-                    if (window.kotlinBridge) kotlinBridge.consoleWarn(formatArgs(args)); 
-                    originalWarn.apply(console, args); 
-                };
-                console.info = function(...args) { 
-                    if (window.kotlinBridge) kotlinBridge.consoleInfo(formatArgs(args)); 
-                    originalInfo.apply(console, args); 
-                };
-            })();
-            """.trimIndent()
-        )
-        logger.debug("Console logging setup completed")
     }
 
     /**
--- src/main/kotlin/io/github/frostzie/datapackide/utils/LoggerProvider.kt
+++ src/main/kotlin/io/github/frostzie/datapackide/utils/LoggerProvider.kt
@@ -1,5 +1,6 @@
 package io.github.frostzie.datapackide.utils
 
+import javafx.scene.web.WebView
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -12,5 +13,52 @@ object LoggerProvider {
         return loggers.getOrPut(fullName) {
             LoggerFactory.getLogger(fullName)
         }
+    }
+
+    /**
+     * Sets up JavaScript console logging to redirect to logger
+     */
+    fun setupConsoleLogging(webView: WebView) {
+        val logger = getLogger("WebView")
+        webView.engine.executeScript(
+            """
+            (function() {
+                const originalLog = console.log;
+                const originalError = console.error;
+                const originalWarn = console.warn;
+                const originalInfo = console.info;
+
+                function formatArgs(args) {
+                    return args.map(arg => {
+                        if (arg instanceof Error) {
+                            return arg.stack || arg.message;
+                        }
+                        if (typeof arg === 'object' && arg !== null) {
+                            try { return JSON.stringify(arg, null, 2); } catch (e) { return '[Unserializable Object]'; }
+                        }
+                        return String(arg);
+                    }).join(' ');
+                }
+
+                console.log = function(...args) { 
+                    if (window.kotlinBridge) kotlinBridge.consoleLog(formatArgs(args)); 
+                    originalLog.apply(console, args); 
+                };
+                console.error = function(...args) { 
+                    if (window.kotlinBridge) kotlinBridge.consoleError(formatArgs(args)); 
+                    originalError.apply(console, args); 
+                };
+                console.warn = function(...args) { 
+                    if (window.kotlinBridge) kotlinBridge.consoleWarn(formatArgs(args)); 
+                    originalWarn.apply(console, args); 
+                };
+                console.info = function(...args) { 
+                    if (window.kotlinBridge) kotlinBridge.consoleInfo(formatArgs(args)); 
+                    originalInfo.apply(console, args); 
+                };
+            })();
+            """.trimIndent()
+        )
+        logger.debug("Console logging setup completed")
     }
 }

56140c794fd5fb637661dae05b2ae26cee9e4a23
Frostzie <136027000+Frostzie@users.noreply.github.com>
2025-11-14 17:29:50 +0200

    Added @ConfigEditorButton to example config as well as rewrote it to be cleaner. Removed examples from MainConfig

--- src/main/kotlin/io/github/frostzie/datapackide/settings/categories/ExampleConfig.kt
+++ src/main/kotlin/io/github/frostzie/datapackide/settings/categories/ExampleConfig.kt
@@ -3,6 +3,7 @@ package io.github.frostzie.datapackide.settings.categories
 import io.github.frostzie.datapackide.settings.KeyCombination
 import io.github.frostzie.datapackide.settings.annotations.ConfigCategory
 import io.github.frostzie.datapackide.settings.annotations.ConfigEditorBoolean
+import io.github.frostzie.datapackide.settings.annotations.ConfigEditorButton
 import io.github.frostzie.datapackide.settings.annotations.ConfigEditorDropdown
 import io.github.frostzie.datapackide.settings.annotations.ConfigEditorKeybind
 import io.github.frostzie.datapackide.settings.annotations.ConfigEditorSlider
@@ -18,63 +19,38 @@ import javafx.scene.input.KeyCode
 object ExampleConfig {
 
     @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "Boolean toggle for true or false decisions.", desc = "Description would show up here this could be very long or short too I will show off how long this can go by rambling about nothing so please enjoy my story about nothing: The story started with nothing the end. Thanks for the read! I hope you enjoyed the story of nothing!")
+    @ConfigCategory(name = "Example", desc = "Example description")
+    @ConfigOption(name = "Boolean", desc = "Description")
     @ConfigEditorBoolean
-    val bool1 = SimpleBooleanProperty(true)
+    val bool = SimpleBooleanProperty(true)
 
     @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "2nd Booolean this is veryyyyyy long so get ready to readddddd lol but not this won't be tooo long just tryyyy to gettttt to the neexxxttt line :)", desc = "Shorty")
-    @ConfigEditorBoolean
-    val bool2 = SimpleBooleanProperty(false)
-
-    @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "LOL000000000000000000000000000000000000000000000000000000000000000000000000000000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", desc = "HALLOghhhhhhhhhhhhhhfhfhfhfghffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
+    @ConfigCategory(name = "Example")
+    @ConfigOption(name = "Keybinds", desc = "Description")
     @ConfigEditorKeybind
-    val key1 = SimpleObjectProperty(KeyCombination(key = KeyCode.S, ctrl = true, alt = true))
+    val key = SimpleObjectProperty(KeyCombination(key = KeyCode.S, ctrl = true, alt = true))
 
     @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "LOL", desc = "HALLO")
-    @ConfigEditorKeybind
-    val key2 = SimpleObjectProperty(KeyCombination(key = KeyCode.S, ctrl = true, alt = true))
-
-    @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "DropdownLONGERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", desc = "DescriptionNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN")
+    @ConfigCategory(name = "Example")
+    @ConfigOption(name = "Dropdown", desc = "Description")
     @ConfigEditorDropdown(values = ["Option 1", "Option 2", "Option 3", "Option 4"])
-    val combo1 = SimpleStringProperty("Option 3")
-
+    val combo = SimpleStringProperty("Option 3")
+    
     @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "Dropdown", desc = "Description is going to go somewhere around this area of my currently typed text!")
-    @ConfigEditorDropdown(values = ["Option 1", "Option 2", "Option 3", "Option 4"])
-    val combo2 = SimpleStringProperty("Option 3")
-
-    @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "Text Areaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", desc = "Descriptioneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+    @ConfigCategory(name = "Example")
+    @ConfigOption(name = "Text field", desc = "Description")
     @ConfigEditorText
-    val text1 = SimpleStringProperty("Example text...")
+    val text = SimpleStringProperty("Example text...")
 
     @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "Text Area", desc = "Description")
-    @ConfigEditorText
-    val text2 = SimpleStringProperty("Example text...")
-
-    @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
-    @ConfigOption(name = "Sliderrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrreteererrrrrrrrrrrrrrrrrrrrrrrereeerrererererreeeeeeeeeeeeeeeeeeeeeeeeeererrrrrrrrrrrrrrrrrerrrrrrrrrrrrreeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrr", desc = "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRiptionnnsssDescription")
-    @ConfigEditorSlider(minValue = 5.0, maxValue = 300.0, stepSize = 5.0)
-    val slider1 = SimpleDoubleProperty(30.0)
-
-    @Expose
-    @ConfigCategory(name = "Examples v2", desc = "These are the example settings that can be used!")
+    @ConfigCategory(name = "Example")
     @ConfigOption(name = "Slider", desc = "Description")
     @ConfigEditorSlider(minValue = 5.0, maxValue = 300.0, stepSize = 5.0)
-    val slider2 = SimpleDoubleProperty(30.0)
+    val slider = SimpleDoubleProperty(30.0)
 
+    @Expose
+    @ConfigCategory(name = "Example")
+    @ConfigOption(name = "Button", desc = "Description")
+    @ConfigEditorButton(text = "Button")
+    val button: () -> Unit = {}
 }
--- src/main/kotlin/io/github/frostzie/datapackide/settings/categories/MainConfig.kt
+++ src/main/kotlin/io/github/frostzie/datapackide/settings/categories/MainConfig.kt
@@ -1,43 +1,4 @@
 package io.github.frostzie.datapackide.settings.categories
 
-import io.github.frostzie.datapackide.settings.KeyCombination
-import io.github.frostzie.datapackide.settings.annotations.*
-import javafx.beans.property.SimpleBooleanProperty
-import javafx.beans.property.SimpleDoubleProperty
-import javafx.beans.property.SimpleObjectProperty
-import javafx.beans.property.SimpleStringProperty
-import javafx.scene.input.KeyCode
-
-
 object MainConfig {
-
-    @Expose
-    @ConfigCategory(name = "Examples", desc = "Example settings")
-    @ConfigOption(name = "True / False", desc = "Description")
-    @ConfigEditorBoolean
-    val example1 = SimpleBooleanProperty(true)
-
-    @Expose
-    @ConfigCategory(name = "Examples")
-    @ConfigOption(name = "Slider", desc = "Description")
-    @ConfigEditorSlider(minValue = 5.0, maxValue = 300.0, stepSize = 5.0)
-    val example2 = SimpleDoubleProperty(30.0)
-
-    @Expose
-    @ConfigCategory(name = "Examples")
-    @ConfigOption(name = "Dropdown", desc = "Description")
-    @ConfigEditorDropdown(values = ["Option 1", "Option 2", "Option 3", "Option 4"])
-    val example3 = SimpleStringProperty("Option 3")
-
-    @Expose
-    @ConfigCategory(name = "Examples")
-    @ConfigOption(name = "Text Area", desc = "Description")
-    @ConfigEditorText
-    val example4 = SimpleStringProperty("Example text...")
-    
-    @Expose
-    @ConfigCategory(name = "Examples")
-    @ConfigOption(name = "Keybind", desc = "Description")
-    @ConfigEditorKeybind
-    val example5 = SimpleObjectProperty(KeyCombination(key = KeyCode.S, ctrl = true, alt = true))
 }

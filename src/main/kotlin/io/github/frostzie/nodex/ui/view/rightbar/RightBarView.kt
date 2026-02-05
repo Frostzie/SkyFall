a358c7e40cbf212e1bf58086dcde5a46a5c5f9b0
Frostzie <136027000+Frostzie@users.noreply.github.com>
2026-02-06 01:14:25 +0200

    Added Rightbar

---
+++ src/main/kotlin/io/github/frostzie/nodex/ui/view/rightbar/RightBarView.kt
@@ -0,0 +1,42 @@
+package io.github.frostzie.nodex.ui.view.rightbar
+
+import atlantafx.base.controls.Spacer
+import atlantafx.base.theme.Styles
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.control.Button
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
+import org.kordamp.ikonli.javafx.FontIcon
+import org.kordamp.ikonli.material2.Material2OutlinedMZ
+
+class RightBarView : VBox() {
+
+    init {
+        prefWidth = 40.0
+        minWidth = 40.0
+        maxWidth = 40.0
+
+        val bottomBtnArea = VBox().apply {
+            alignment = Pos.TOP_CENTER
+
+
+            val notifBtn = Button(null, FontIcon(Material2OutlinedMZ.NOTIFICATIONS_NONE))
+            notifBtn.styleClass.addAll(
+                Styles.FLAT,
+                Styles.BUTTON_ICON
+            )
+
+            notifBtn.onAction
+
+
+            children.add(notifBtn)
+            padding = Insets(2.0)
+        }
+
+        val spacer = Spacer()
+        setVgrow(spacer, Priority.ALWAYS)
+
+        children.addAll(spacer, bottomBtnArea)
+    }
+}

8b5ac4620ba96d2a1379fc1178c811fe94cafa37
Frostzie <136027000+Frostzie@users.noreply.github.com>
2026-02-06 02:02:51 +0200

    Removed legacy Bar classes. (LeftBar And BottomBar views, modules, handlers)

--- src/main/kotlin/io/github/frostzie/nodex/handlers/bars/LeftBarHandler.kt
+++
@@ -1,8 +0,0 @@
-package io.github.frostzie.nodex.handlers.bars
-
-import io.github.frostzie.nodex.modules.bars.LeftBarModule
-
-@Suppress("unused")
-class LeftBarHandler(private val leftBarModule: LeftBarModule) {
-
-}

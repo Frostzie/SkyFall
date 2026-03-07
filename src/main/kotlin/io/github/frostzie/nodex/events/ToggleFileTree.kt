51ae8f16a1cc4b3d28356ddeb8fd348af12fd52c
Frostzie <136027000+Frostzie@users.noreply.github.com>
2026-03-07 23:56:10 +0200

    Removing about all the legacy files. (Some were deleted sooo long ago lol)

--- src/main/kotlin/io/github/frostzie/nodex/events/ToggleFileTree.kt
+++
@@ -1,3 +0,0 @@
-package io.github.frostzie.nodex.events
-
-class ToggleFileTree
--- src/main/kotlin/io/github/frostzie/nodex/settings/annotations/SubscribeEvent.kt
+++
@@ -1,5 +0,0 @@
-package io.github.frostzie.nodex.settings.annotations
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class SubscribeEvent

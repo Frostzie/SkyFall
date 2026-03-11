4a22123d24059da08381bbccf81a26dac3ecf97d
Frostzie <136027000+Frostzie@users.noreply.github.com>
2026-03-11 22:37:55 +0200

    Changed donate link from Buy Me a coffee to Ko-fi.

--- src/main/kotlin/io/github/frostzie/nodex/domain/uicontract/Links.kt
+++ src/main/kotlin/io/github/frostzie/nodex/domain/uicontract/Links.kt
@@ -4,7 +4,7 @@ object Links {
     const val DISCORD = "https://discord.gg/qZ885qTvkx"
     const val GITHUB = "https://github.com/Frostzie/Nodex"
     const val ISSUES = "https://github.com/Frostzie/Nodex/issues"
-    const val DONATE = "https://www.buymeacoffee.com/frostzie"
+    const val DONATE = "https://ko-fi.com/frostzie"
     const val MODRINTH = "https://modrinth.com/project/XlilVGvF"
     const val WIKI = "https://github.com/Frostzie/Nodex/wiki" //TODO: eventually
     const val CHANGELOGS = "https://github.com/Frostzie/Nodex/blob/main/docs/CHANGELOG.md" //

9b8ed87d1e149f8bd5ca8ed354e3142785ca53c2
Frostzie <136027000+Frostzie@users.noreply.github.com>
2026-03-21 20:24:37 +0200

    Removed the left test tools from ToolWindow.

--- src/main/kotlin/io/github/frostzie/nodex/domain/uicontract/ToolWindow.kt
+++ src/main/kotlin/io/github/frostzie/nodex/domain/uicontract/ToolWindow.kt
@@ -4,6 +4,5 @@ package io.github.frostzie.nodex.domain.uicontract
  * Defines the available types of tool windows in the workbench.
  */
 enum class ToolWindow {
-    FILES,
-    TEST_RIGHT
+    FILES
 }

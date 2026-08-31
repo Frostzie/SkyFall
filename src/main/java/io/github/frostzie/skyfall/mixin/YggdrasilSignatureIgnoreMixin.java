c751afbf4b2a6c0f87fa69a040abf55509e4419a
Frostzie <136027000+Frostzie@users.noreply.github.com>
2026-08-31 03:10:52 +0300

    Added YggdrasilSignatureIgnoreMixin so logs aren't being spammed anymore. (Credits: Odin)

---
+++ src/main/java/io/github/frostzie/skyfall/mixin/YggdrasilSignatureIgnoreMixin.java
@@ -0,0 +1,19 @@
+package io.github.frostzie.skyfall.mixin;
+
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.yggdrasil.YggdrasilServicesKeyInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+// Taken from Odin
+// https://github.com/odtheking/Odin/blob/main/src/main/java/com/odtheking/mixin/mixins/YggdrasilSignatureIgnoreMixin.java
+@Mixin(value = YggdrasilServicesKeyInfo.class, remap = false)
+public class YggdrasilSignatureIgnoreMixin {
+
+    @Inject(method = "validateProperty", at = @At("HEAD"), cancellable = true, remap = false)
+    public void validate(Property property, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
